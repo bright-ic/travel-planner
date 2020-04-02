0b0ddccbe0d575bfa18d07d8e5fb6d2455708cab
Bright Onwukwe <onwukweb@gmail.com>
2020-04-02 16:18:26 +0100

    Added trip city search and date ui

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,11 @@
 import { handleSubmit } from './js/app';
+import "./styles/resets.scss";
 import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
-import "./styles/resets.scss";
+import "./styles/tripInfo.scss";
+import "./styles/flatpickr.min.scss";
 
 
 export {
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,4 +1,4 @@
-
+import flatpickr from "flatpickr";
 /* Global Variables */
 const geonameBaseUrl = "http://api.geonames.org/searchJSON?name_equals=";
 const geonameUser = "onwukweb";
@@ -22,26 +22,65 @@ const getCityLocation = async (url) => {
 }
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 // helper function that handles form submission
-export const handleSubmit = async (city) => {
+export const handleSubmit = async (city, date) => {
    try {
      if(city === "") {
        throw new Error("Please enter city name");
      }
-    const res = await getCityLocation(`${geonameBaseUrl}${city}&username=${geonameUser}`);
-    console.log(res);
+     if(date === "" ) {
+        throw new Error("Pick a travel date");
+      }
+     
+    let tripDate = getTripDate(date); // get the travel start date and [end return date if a range is selected]
+    console.log("trip dates",tripDate);
+
+      const res = await getCityLocation(`${geonameBaseUrl}${city}&username=${geonameUser}`);
+      console.log(res);
    }
    catch(err) {
     alert(err.message);
    }
 }
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+// Helper function that gets the users travel duration
+const getTravelDuration = (startDate, endDate) => {
+    // To set two dates to two variables 
+    const date1 = new Date(startDate); 
+    const date2 = new Date(endDate); 
 
+    let Difference_In_Time = date2.getTime() - date1.getTime(); 
+    let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+    return Difference_In_Days;
+}
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+const getTripDate = (tdate) => {
+  const trip = {travelDate:'', returnDate:'', duration:''};
+  if(tdate === "") {
+    return null;
+  }
+
+  trip.travelDate = tdate;
+  if(tdate.indexOf("to") > 0) {
+    const datesArr = tdate.split(" ");
+    trip.travelDate = datesArr[0];
+    trip.returnDate = datesArr[2];
+    trip.duration = getTravelDuration(trip.travelDate, trip.returnDate); // get travel duration
+  }
+  return trip;
+}
 
 
 //Event listeners
 document.querySelector("#form").addEventListener("submit", function(event) {
   event.preventDefault();
   const city = document.getElementById("city").value;
-  return Client.handleSubmit(city);
+  let date = document.getElementById("startDate").value;
+  return Client.handleSubmit(city, date);
   return false;
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  flatpickr(".date", {mode:"range"});
+
+    console.log("dom is ready");
+})
